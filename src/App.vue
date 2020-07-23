b7fc4087b67a0e52fd72d80322821eec74112813
Kaique Garcia <kaiquegarciam@gmail.com>
2020-07-23 16:15:24 -0300

    Criação de workspace de tabelas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+    <h1>Modelator.io</h1>
+    <br />
+    <b-button variant="success" @click="createTable">
+      <b-icon-plus></b-icon-plus> Adicionar tabelas
+    </b-button>
+    <hr />
+    <div ref="workspace"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DatabaseTable from "./components/database/DatabaseTable";
+import Vue from "vue";
 
+const DatabaseTableClass = Vue.extend(DatabaseTable);
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tables: {},
+      instances: {},
+      lastId: 0
+    };
+  },
+  methods: {
+    createTable() {
+      let tableContainer = document.createElement("div");
+      this.lastId++;
+      let id = "table_" + this.lastId;
+      tableContainer.id = id;
+      this.$refs.workspace.appendChild(tableContainer);
+      this.tables[id] = {
+        name: "table_name",
+        description: "",
+        engine: "",
+        columns: [
+          {
+            name: "id",
+            dataType: "INT(11)",
+            defaultExpression: "",
+            PK: true,
+            AI: true,
+            NN: false,
+            UQ: false,
+            UN: false,
+            ZF: false,
+            B: false
+          },
+          {
+            name: "status",
+            dataType: "ENUM('draft', 'published')",
+            defaultExpression: "'draft'",
+            PK: false,
+            AI: false,
+            NN: true,
+            UQ: false,
+            UN: false,
+            ZF: false,
+            B: false
+          },
+          {
+            name: "deleted_at",
+            dataType: "DATETIME",
+            defaultExpression: "NULL",
+            PK: false,
+            AI: false,
+            NN: false,
+            UQ: false,
+            UN: false,
+            ZF: false,
+            B: false
+          },
+          {
+            name: "updated_at",
+            dataType: "DATETIME",
+            defaultExpression: "NULL",
+            PK: false,
+            AI: false,
+            NN: false,
+            UQ: false,
+            UN: false,
+            ZF: false,
+            B: false
+          },
+          {
+            name: "created_at",
+            dataType: "TIMESTAMP",
+            defaultExpression: "CURRENT_TIMESTAMP",
+            PK: false,
+            AI: false,
+            NN: false,
+            UQ: false,
+            UN: false,
+            ZF: false,
+            B: false
+          }
+        ],
+        indexes: [],
+        foreignKeys: []
+      };
+      let table = new DatabaseTableClass({
+        propsData: {
+          tableInput: this.tables[id]
+        }
+      });
+      table.$mount("#table_" + this.lastId);
+      table.$on("remove", () => this.removeTable(id));
+      this.instances[id] = table;
+    },
+    removeTable(id) {
+      this.instances[id].$destroy();
+      delete this.instances[id];
+      delete this.tables[id];
+    }
   }
 };
 </script>
@@ -21,7 +123,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/database/DatabaseTable.vue
+++ src/components/database/DatabaseTable.vue
@@ -1,83 +1,82 @@
 <template>
   <div class="database-table">
-    <b-card no-body>
-      <b-tabs v-model="currentTab" card>
-        <b-tab title="Columns">
-          <b-card-text>
-            <table
-              role="table"
-              class="table b-table table-striped table-hover table-bordered table-sm table-light"
-            >
-              <thead role="rowgroup" class="thead-dark">
-                <tr role="row">
-                  <th role="columnheader" scope="col" v-if="table.engine" class="bg-danger align-middle">
-                    <div>
-                      {{ table.engine }}
-                    </div>
-                  </th>
-                  <th role="columnheader" scope="col" :colspan="table.engine ? 3 : 4">
-                    <div>
-                      {{ table.name }}
-                      <b-icon-pencil
-                        class="cursor-pointer"
-                        @click="openTableEditor"
-                      ></b-icon-pencil>
-                      <template v-if="table.description">
-                        <br />
-                        <small>{{ table.description }}</small>
-                      </template>
-                    </div>
-                  </th>
-                </tr>
-                <tr role="row">
-                  <th role="columnheader" scope="col" title="Primary Key">
-                    <div>PK</div>
-                  </th>
-                  <th role="columnheader" scope="col">
-                    <div>Name</div>
-                  </th>
-                  <th role="columnheader" scope="col">
-                    <div>Data Type</div>
-                  </th>
-                  <th role="columnheader" scope="col">
-                    <b-icon-plus
-                      class="cursor-pointer"
-                      @click="createColumn"
-                    ></b-icon-plus>
-                  </th>
-                </tr>
-              </thead>
-              <draggable v-model="columns" tag="tbody" role="rowgroup">
-                <tr
-                  v-for="(column, columnIndex) in columns"
-                  role="row"
-                  :key="columnIndex"
-                >
-                  <td role="cell">
-                    <b-icon-lock v-if="column.PK"></b-icon-lock>
-                  </td>
-                  <td role="cell">{{ column.name }}</td>
-                  <td role="cell">{{ column.dataType }}</td>
-                  <td role="cell">
-                    <b-icon-pencil
-                      class="cursor-pointer"
-                      @click="openColumnEditor(columnIndex)"
-                    ></b-icon-pencil>
-                    <b-icon-x
-                      class="cursor-pointer"
-                      @click="removeColumn(columnIndex)"
-                    ></b-icon-x>
-                  </td>
-                </tr>
-              </draggable>
-            </table>
-          </b-card-text>
-        </b-tab>
-        <b-tab title="Table related indexes">
-          <b-card-text></b-card-text>
-        </b-tab>
-      </b-tabs>
-    </b-card>
+    <table
+      role="table"
+      class="table b-table table-striped table-hover table-bordered table-sm table-light"
+    >
+      <thead role="rowgroup" class="thead-dark text-center">
+        <tr role="row">
+          <th
+            role="columnheader"
+            scope="col"
+            v-if="table.engine"
+            class="bg-danger align-middle"
+          >
+            <div>
+              {{ table.engine }}
+            </div>
+          </th>
+          <th role="columnheader" scope="col" :colspan="table.engine ? 2 : 3">
+            <div>
+              {{ table.name }}
+              <b-icon-pencil
+                class="cursor-pointer"
+                @click="openTableEditor"
+              ></b-icon-pencil>
+              <template v-if="table.description">
+                <br />
+                <small>{{ table.description }}</small>
+              </template>
+            </div>
+          </th>
+          <th role="columnheader" scope="col">
+            <div>
+              <b-icon-x class="cursor-pointer" @click="remove"></b-icon-x>
+            </div>
+          </th>
+        </tr>
+        <tr role="row">
+          <th role="columnheader" scope="col" title="Primary Key">
+            <div>PK</div>
+          </th>
+          <th role="columnheader" scope="col">
+            <div>Name</div>
+          </th>
+          <th role="columnheader" scope="col">
+            <div>Data Type</div>
+          </th>
+          <th role="columnheader" scope="col">
+            <b-icon-plus
+              class="cursor-pointer"
+              @click="createColumn"
+            ></b-icon-plus>
+          </th>
+        </tr>
+      </thead>
+      <draggable v-model="table.columns" tag="tbody" role="rowgroup">
+        <tr
+          v-for="(column, columnIndex) in table.columns"
+          role="row"
+          :key="columnIndex"
+        >
+          <td role="cell" class="text-center">
+            <b-icon-lock v-if="column.PK"></b-icon-lock>
+          </td>
+          <td role="cell">{{ column.name }}</td>
+          <td role="cell">{{ column.dataType }}</td>
+          <td role="cell" class="text-center">
+            <b-icon-pencil
+              class="cursor-pointer"
+              @click="openColumnEditor(columnIndex)"
+            ></b-icon-pencil>
+            <b-icon-x
+              class="cursor-pointer"
+              @click="removeColumn(columnIndex)"
+            ></b-icon-x>
+          </td>
+        </tr>
+      </draggable>
+    </table>
     <div ref="editorContainer"></div>
   </div>
 </template>
@@ -95,78 +94,15 @@ export default {
   components: {
     draggable
   },
+  props: {
+    tableInput: {
+      type: Object,
+      required: true
+    }
+  },
   data() {
     return {
-      table: {
-        name: "table_name",
-        description: "",
-        engine: ""
-      },
-      columns: [
-        {
-          name: "id",
-          dataType: "INT(11)",
-          defaultExpression: "",
-          PK: true,
-          AI: true,
-          NN: false,
-          UQ: false,
-          UN: false,
-          ZF: false,
-          B: false
-        },
-        {
-          name: "status",
-          dataType: "ENUM('draft', 'published')",
-          defaultExpression: "'draft'",
-          PK: false,
-          AI: false,
-          NN: true,
-          UQ: false,
-          UN: false,
-          ZF: false,
-          B: false
-        },
-        {
-          name: "deleted_at",
-          dataType: "DATETIME",
-          defaultExpression: "NULL",
-          PK: false,
-          AI: false,
-          NN: false,
-          UQ: false,
-          UN: false,
-          ZF: false,
-          B: false
-        },
-        {
-          name: "updated_at",
-          dataType: "DATETIME",
-          defaultExpression: "NULL",
-          PK: false,
-          AI: false,
-          NN: false,
-          UQ: false,
-          UN: false,
-          ZF: false,
-          B: false
-        },
-        {
-          name: "created_at",
-          dataType: "TIMESTAMP",
-          defaultExpression: "CURRENT_TIMESTAMP",
-          PK: false,
-          AI: false,
-          NN: false,
-          UQ: false,
-          UN: false,
-          ZF: false,
-          B: false
-        }
-      ],
-      indexes: [],
-      foreignKeys: [],
-      currentTab: 0,
+      table: this.tableInput,
       currentEditingColumnIndex: -1
     };
   },
@@ -193,7 +129,7 @@ export default {
       this.$refs.editorContainer.appendChild(editorContainer);
       columnEditor = new ColumnEditorClass({
         propsData: {
-          columnInput: this.columns[this.currentEditingColumnIndex]
+          columnInput: this.table.columns[this.currentEditingColumnIndex]
         }
       });
       columnEditor.$mount("#column-editor-container");
@@ -203,9 +139,9 @@ export default {
       columnEditor.$destroy();
     },
     createColumn() {
-      let firstColumn = !this.columns.length;
+      let firstColumn = !this.table.columns.length;
       let column = {
-        name: firstColumn ? "id" : "row_" + this.columns.length,
+        name: firstColumn ? "id" : "row_" + this.table.columns.length,
         dataType: firstColumn ? "INT" : "VARCHAR(45)",
         defaultExpression: "",
         PK: Boolean(firstColumn),
@@ -216,22 +152,29 @@ export default {
         ZF: false,
         B: false
       };
-      this.columns.push(column);
-      this.openColumnEditor(this.columns.length - 1);
+      this.table.columns.push(column);
+      this.openColumnEditor(this.table.columns.length - 1);
     },
     removeColumn(columnIndex) {
-      this.columns.splice(columnIndex, 1);
+      this.table.columns.splice(columnIndex, 1);
+    },
+    remove() {
+      this.$emit("remove");
     }
+  },
+  beforeDestroy() {
+    this.$root.$el.parentNode.removeChild(this.$root.$el);
   }
 };
 </script>
 <style lang="sass">
-.database-table table
-    width: 450px !important
-    max-width: 100%
-
-    tbody tr
-        cursor: move
+.database-table
+    margin-bottom: 30px
+    table
+        width: 450px !important
+        max-width: 100%
+        tbody tr
+            cursor: move
 
 .cursor-pointer
     cursor: pointer
